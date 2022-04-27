0cfae7b2d5467f29114f84d2c007ae58d3991f89
Elon Barzani <elon5000@gmail.com>
2022-04-27 11:40:56 +0300

    mail service

---
+++ apps/email/services/email-service.js
@@ -0,0 +1,100 @@
+import { storageService } from './storage.service.js'
+import { utilService } from './util.service.js'
+
+export const carService = {
+    getById,
+    query,
+    saveDraft,
+    deleteEmail,
+    getNextEmailId
+}
+
+const KEY = 'emailDB'
+
+function query(filterBy) {
+    let emails = _loadFromStorage()
+    if (!emails) {
+        emails = _createEmails()
+        _saveToStorage(emails)
+    }
+
+    if (filterBy) {
+        let { vendor, minSpeed, maxSpeed } = filterBy
+        if (!minSpeed) minSpeed = 0;
+        if (!maxSpeed) maxSpeed = Infinity
+        cars = cars.filter(car =>
+            car.vendor.includes(vendor) &&
+            car.speed <= maxSpeed &&
+            car.speed >= minSpeed)
+    }
+
+    return Promise.resolve(cars)
+}
+
+function getNextEmailId(emailId) {
+    const emails = _loadFromStorage()
+    const emailIdx = emails.findIndex(email => emailId === email.id)
+    const nextEmailIdx = (emailIdx + 1 === email.length) ? 0 : emailIdx + 1
+    return emails[nextEmailIdx].id
+}
+
+function getById(emailId) {
+    const emails = _loadFromStorage()
+    const email = emails.find(email => emailId === email.id)
+    return Promise.resolve(email)
+}
+
+function deleteEmail(emailId) {
+    let emails = _loadFromStorage()
+    emails = emails.filter(email => email.id !== emailId)
+    _saveToStorage(emails)
+    return Promise.resolve()
+}
+
+function saveDraft(email) {
+    if (email.id) return _update(email)
+    else return _add(email)
+}
+
+function _add(emailToAdd) {
+    let emails = _loadFromStorage()
+    const email = _createCar(emailToAdd.to, emailToAdd.subject, emailToAdd.content)
+    emails = [email, ...emails]
+    _saveToStorage(emails)
+    return Promise.resolve()
+}
+
+function _update(emailToUpdate) {
+    let emails = _loadFromStorage()
+    emails = emails.map(email => email.id === emailToUpdate.id ? emailToUpdate : email)
+    _saveToStorage(emails)
+    return Promise.resolve()
+}
+
+
+function _composeEmail(to, subject, content) {
+    return {
+        id: utilService.makeId(),
+        to,
+        subject,
+        content,
+        ctg: (Math.random() > 0.5) ? 'Important' : 'Sales'
+    }
+}
+
+function _createEmails() {
+    const emails = []
+    for (let i = 0; i < 10; i++) {
+        const vendor = gVendors[utilService.getRandomIntInclusive(0, gVendors.length - 1)]
+        cars.push(_createCar(vendor))
+    }
+    return cars
+}
+
+function _saveToStorage(cars) {
+    storageService.saveToStorage(KEY, cars)
+}
+
+function _loadFromStorage() {
+    return storageService.loadFromStorage(KEY)
+}
