88fdd93c3617b25b061191460ebc6a05e350daa4
Elon Barzani <elon5000@gmail.com>
2022-04-27 17:26:17 +0300

    keep fake data

---
+++ apps/keep/data.js
@@ -0,0 +1,38 @@
+const keepsData = [
+    {
+        id: 'Hy98B',
+        type: 'note-img',
+        subject: 'Bobi and me',
+        txt: '',
+        file: '../../../assets/pages/keep/file-storage/imgs/img1.jpg',
+        backgroundColor: 'lightblue'
+    },
+    {
+        id: 'z6Po0',
+        type: 'note-list',
+        subject: 'Shopping list',
+        txt: {
+            text1: 'milk',
+            text2: 'cookies',
+            text3: 'toiletpaper'
+        },
+        file: '',
+        backgroundColor: 'lightgreen'
+    },
+    {
+        id: '4uY5e',
+        type: 'note-txt',
+        subject: 'My poem',
+        txt: 'Roses are red, violets are blue',
+        file: '',
+        backgroundColor: 'lightyellow'
+    },
+    {
+        id: 'G6ua7',
+        type: 'note-video',
+        subject: 'My favroite video',
+        txt: 'lol its so funny XD',
+        file: '../../../assets/pages/keep/file-storage/vids/vid1.mp4',
+        backgroundColor: 'lightpurple'
+    },
+]
---
+++ apps/keep/services/keep.service.js
@@ -0,0 +1,2 @@
+import { storageService } from '../../../services/storage.service.js'
+import { utilService } from '../../../services/util.service.js'
