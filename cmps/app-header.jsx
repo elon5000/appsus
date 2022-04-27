cb51b6fd53f5a50e5dbd7ebc63f76187d26fb3c6
eldadtishlerca <eldad55555@gmail.com>
2022-04-27 13:06:18 +0300

    style navbar

--- cmps/app-header.jsx
+++ cmps/app-header.jsx
@@ -2,9 +2,11 @@ export function AppHeader() {
   return (
     <section className="app-header">
       <div className="logo-wrapper">
+        <h1>Appsus</h1>
+      </div>
+      <div className="navbar-wrapper flex align-center">
         <img src="../assets/imgs/navbar.png" alt="" />
       </div>
-      <nav className="navbar"></nav>
     </section>
   )
 }
