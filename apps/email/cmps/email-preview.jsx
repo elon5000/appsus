6157caa02ae189dc48bd18dd86d3d7c5527efb72
Elon Barzani <elon5000@gmail.com>
2022-04-27 15:25:44 +0300

    attachments works

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -6,8 +6,8 @@ export function EmailPreview({ email }) {
         <h3>From : {email.to}</h3>
         <h3>Subject : {email.subject}</h3>
         <h5>Content : {email.body}</h5>
-        <div className="img-container">
-          <img src={email.file} />
+        <div className="attachment-container">
+        {(email.file) && <img src="./assets/imgs/attachment.png" /> }
         </div>
       </article>
     </Link>
