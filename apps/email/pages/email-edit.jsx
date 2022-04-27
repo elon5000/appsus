9a52357377bb3881b7c37142f2f4746b9578db7a
eldadtishlerca <eldad55555@gmail.com>
2022-04-27 16:18:07 +0300

    edit draft

--- apps/email/pages/email-edit.jsx
+++ apps/email/pages/email-edit.jsx
@@ -2,21 +2,73 @@ import { emailService } from '../services/email-service.js'
 
 export class EmailEdit extends React.Component {
   state = {
-    email: {},
+    email: {
+      subject: '',
+      body: '',
+      to: '',
+    },
   }
-}
 
-handleChange = ({ target }) => {
-  const field = target.name
-  const value = target.value
-  this.setState((prevState) => ({
-    email: { ...prevState.email, [field]: value },
-  }))
+  timeoutId
+
+  componentDidMount() {
+    if (this.timeoutId) clearTimeout(this.timeoutId)
+    this.timeoutId = setTimeout(this.onSaveDraft, 5000)
+  }
+
+  handleChange = ({ target }) => {
+    const field = target.name
+    const value = target.value
+    this.setState((prevState) => ({
+      email: { ...prevState.email, [field]: value },
+    }))
+  }
 
   onSaveEmail = (ev) => {
     ev.preventDefault()
     emailService.saveEmail(this.state.email).then(() => {
       this.props.history.push('/email')
     })
+  }
+
+  onSaveDraft()
+
+  render() {
+    const { subject, body, to, file } = this.state.email
+    return (
+      <section className="email-edit flex column align-center">
+        <h2>Send new email</h2>
+        <form className="email-form" onSubmit={this.onSaveEmail}>
+          <input
+            type="text"
+            name="to"
+            onChange={this.handleChange}
+            placeholder="To:"
+            value={to}
+          />
+          <input
+            type="text"
+            name="subject"
+            onChange={this.handleChange}
+            placeholder="Enter subject"
+            value={subject}
+          />
+          <input
+            type="text"
+            name="body"
+            onChange={this.handleChange}
+            placeholder="Your text"
+            value={body}
+          />
+          {/* <input
+            type="text"
+            name="file"
+            onChange={this.handleChange}
+            value={file}
+          /> */}
+          <button>Submit</button>
+        </form>
+      </section>
+    )
   }
 }
