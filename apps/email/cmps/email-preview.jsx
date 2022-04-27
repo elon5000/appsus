a69405296134092dc0518512db42b0720374dc25
Elon Barzani <elon5000@gmail.com>
2022-04-27 16:40:55 +0300

    more ccs

--- apps/email/cmps/email-preview.jsx
+++ apps/email/cmps/email-preview.jsx
@@ -3,15 +3,15 @@ export function EmailPreview({ email }) {
   return (
     <Link to={`/email/${email.id}`}>
       <article className="email-preview">
-        <h3>{email.to}</h3>
-        <h4>Subject : {email.subject}</h4>
-        <h5>Content : {email.body}</h5>
-        <div className="attachment-container">
-          {(email.file) && <span className="attachment-span">
+        <h2>{email.to}</h2>
+        <h3>Subject : {email.subject}</h3>
+        <h4>{email.body}</h4>
+        {(email.file) && <div className="attachment-container">
+          <span className="attachment-span">
             <img src="./assets/imgs/attachment.png" />
             <span>{email.file.slice(46)}</span>
-          </span>}
-        </div>
+          </span>
+        </div>}
       </article>
     </Link>
   )
