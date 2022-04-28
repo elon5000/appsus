4e393c3376490e7f830f6929f0b2d02fcedda01c
Elon Barzani <elon5000@gmail.com>
2022-04-28 11:14:17 +0300

    fixed counter

--- apps/email/cmps/email-counter.jsx
+++ apps/email/cmps/email-counter.jsx
@@ -1,11 +1,13 @@
-export function EmailCounter({ emails }) {
-    this.state = {
-        inboxMail: emails.map(email => email.category === 'inbox'),
-        inboxUnreadMail: inboxMail.map(email => !email.isRead)
-    }
-    console.log('All mail:',this.state.inboxMail, 'Unread mail',this.state.inboxUnreadMail)
+import { emailService } from '../services/email-service.js'
+
+export function EmailCounter() {
+    const inboxEmails = emailService.query('inbox')
+        .then(this.state = {
+            inboxUnreadMail: inboxEmails.map(email => !email.isRead)
+        })
+    console.log('All mail:', inboxEmails.length, 'Unread mail', this.state.inboxUnreadMail)
 
     return <section className="email-counter">
-        <h2>Emails read {inboxMail.length}/{inboxUnreadMail.length} </h2>
+        <h2>Emails read {inboxEmails.length}/{inboxUnreadMail.length}</h2>
     </section>
 }
