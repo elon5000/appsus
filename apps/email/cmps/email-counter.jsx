5ef4e5e9026779331d223a19b4d68a33e37adf2d
eldadtishlerca <eldad55555@gmail.com>
2022-04-29 15:58:41 +0300

    fix counter

--- apps/email/cmps/email-counter.jsx
+++ apps/email/cmps/email-counter.jsx
@@ -1,14 +1,16 @@
 export function EmailCounter({ emails }) {
-  let getEmails = Math.floor(
+  const getEmails = Math.floor(
     ((emails.length - emails.filter((email) => email.isRead === false).length) *
       100) /
       emails.length
   )
 
   return (
-    <section className="email-counter" style={{ width: `${getEmails}%` }}>
+    <section className="email-counter">
       <div className="email-progress-wrapper">
-        <div className="progress-inside">{getEmails}%</div>
+        <div className="progress-inside" style={{ width: `${getEmails}%` }}>
+          {getEmails}%
+        </div>
       </div>
     </section>
   )
