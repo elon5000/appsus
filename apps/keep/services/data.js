0f52e75e4a63203423342b16d6f748c4cd43a0e2
eldadtishlerca <eldad55555@gmail.com>
2022-05-01 07:34:02 +0300

    try fix git

--- apps/keep/services/data.js
+++ apps/keep/services/data.js
@@ -3,19 +3,19 @@ const keepsData = [
     id: 'Hy98B',
     subject: 'Bobi and me',
     txt: '',
-    file: '../../../assets/pages/keep/file-storage/imgs/image1.jpg',
+    file: '/assets/pages/keep/file-storage/imgs/image1.jpg',
     backgroundColor: 'rgb(66 133 244)',
     isPin: false,
-    todoData: ''
+    todoData: '',
   },
   {
     id: '5UPi2',
     subject: 'Test Rec',
     txt: 'Do not try it works!!',
-    file: '../../../assets/pages/keep/file-storage/audio/audio1.m4a',
+    file: '/assets/pages/keep/file-storage/audio/audio1.m4a',
     backgroundColor: 'rgb(219 68 55)',
     isPin: false,
-    todoData: ''
+    todoData: '',
   },
   {
     id: 'z6Po0',
@@ -25,9 +25,9 @@ const keepsData = [
     backgroundColor: 'rgb(244 180 0)',
     isPin: false,
     todoData: {
-      x5Y: {txt: 'buy milk', isMarked: true},
-      zL7: {txt: 'buy coockies', isMarked: false}
-    }
+      x5Y: { txt: 'buy milk', isMarked: true },
+      zL7: { txt: 'buy coockies', isMarked: false },
+    },
   },
   {
     id: '4uY5e',
@@ -36,16 +36,16 @@ const keepsData = [
     file: '',
     backgroundColor: 'rgb(15 157 88)',
     isPin: false,
-    todoData: ''
+    todoData: '',
   },
   {
     id: 'G6ua7',
     subject: 'My favroite video',
     txt: 'lol its so funny XD',
-    file: '../../../assets/pages/keep/file-storage/vids/video1.mp4',
+    file: '/assets/pages/keep/file-storage/vids/video1.mp4',
     backgroundColor: 'rgb(66 133 244)',
     isPin: false,
-    todoData: ''
+    todoData: '',
   },
 ]
 
